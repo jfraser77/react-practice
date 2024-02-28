c1574545860ff4bc5982e837a2764838720bb24e
Joe Fraser <joefraser7@gmail.com>
2024-02-28 13:12:36 -0600

    tic tac toe app complete

--- digital-ocean-vite/src/App.jsx
+++ digital-ocean-vite/src/App.jsx
@@ -1,9 +1,13 @@
-import Welcome from "./components/Welcome"
+
+import Game from "./components/Board";
+import Board from "./components/Board";
 
 export default function App() {
     return (
-    <>
-    <Welcome /> 
-    </>
-    );
+      <>
+        
+        <Game />
+
+      </>
+     );
     }
