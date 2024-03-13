bd319c369e51a686c9e988778231c74108a4e2d3
Joe Fraser <joefraser7@gmail.com>
2024-03-13 13:33:00 -0500

    Inventory and Ravenous App Projects

--- my-blog/src/App.jsx
+++ my-blog/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import ArticlePage from './pages/ArticlePage'
+import ArticlesListPage from './pages/ArticlesListPage'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+    <div className='App'>
+        <h1>My Awesome Blog</h1>
+        <div id='page-body'>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/articles"element={<ArticlesListPage />} />
+            <Route path="/articles/:articleId"element={<ArticlePage />} />
+          </Routes>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </BrowserRouter>
+
     </>
   )
 }
 
-export default App
+export default App;
